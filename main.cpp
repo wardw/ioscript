264e6a9db168cc28e630e49970133767009b7b83
Tom Ward <wardw@users.noreply.github.com>
2016-08-20 21:54:38 +0100

    Early progress

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <vector>
+
+
 
 #include "qplot.h"
 
 using namespace std;
 
+#include <iostream>
 
 int main()
 {
+    // using T = std::tuple<int, double, char>;
+
+    // std::cout << std::boolalpha;
+
+    // std::cout << tuple_contains_type<int, T>::value << std::endl;
+    // std::cout << tuple_contains_type<double, T>::value << std::endl;
+    // std::cout << tuple_contains_type<char, T>::value << std::endl;
+
+    // std::cout << tuple_contains_type<float, T>::value << std::endl;
+    // std::cout << tuple_contains_type<short, T>::value << std::endl;
+    // std::cout << tuple_contains_type<long, T>::value << std::endl;
+
+    // std::cout << std::is_same<tuple_contains_type<int, T>, std::true_type>::value << std::endl;
+
+
+	std::vector<int> ints = {1,2,3,4,5,6,7};
+
+
 	Qplot qplot;
+	qplot.plot(ints, ImageSize{400,300});
 
 	qplot << "reset\n";
 }
--- qplot.h
+++ qplot.h
@@ -1,13 +1,57 @@
 #pragma once
 
+#include <tuple>
+#include <type_traits>
+
 #include "gnuplot.h"
 
+
+struct image_size_canvas_property{};
+
+struct ImageSize
+{
+	using CanvasType = image_size_canvas_property;
+
+	std::string plotString() {
+		return "set terminal pngcairo size " + std::to_string(x) + "," + std::to_string(y) + "\n";
+	}
+	int x = 800;
+	int y = 640;
+};
+
+struct grid_object_style : std::string {};
+
+struct HeatMapStyle
+{
+	using ObjectStyle = grid_object_style;
+};
+
+
+// http://stackoverflow.com/a/25958302/254035
+template <typename T, typename Tuple>
+struct has_type;
+
+template <typename T>
+struct has_type<T, std::tuple<>> : std::false_type {};
+
+template <typename T, typename... Ts>
+struct has_type<T, std::tuple<T, Ts...>> : std::true_type {};
+
+template <typename T, typename U, typename... Ts>
+struct has_type<T, std::tuple<U, Ts...>> : has_type<T, std::tuple<Ts...>> {};
+
+template <typename T, typename Tuple>
+using tuple_contains_type = typename has_type<T, Tuple>::type;
+
+std::tuple<image_size_canvas_property,
+           grid_object_style
+          > styleString{};
 
 class Qplot
 {
 public:
-    enum { DRY_RUN, STD_OUT, GNUPLOT };
-    static constexpr int target = STD_OUT;
+    enum { DRY_RUN, STDOUT, GNUPLOT };
+    static constexpr int target = STDOUT;
 
 	friend Qplot& operator<<(Qplot& qplot, const std::string& str);
 
@@ -16,18 +60,47 @@ public:
         switch (target)
         {
             case DRY_RUN: gnuplot_ = std::make_unique<Gnuplot>("cat > /dev/null");  break;
-            case STD_OUT: gnuplot_ = std::make_unique<Gnuplot>("cat");              break;
+            case STDOUT:  gnuplot_ = std::make_unique<Gnuplot>("cat");              break;
             case GNUPLOT: gnuplot_ = std::make_unique<Gnuplot>("gnuplot");          break;
             default : assert(false);
         }
     }
 
+    template <typename T, typename... Ts>
+    void processArgs(const T& arg, const Ts&... args)
+    {
+
+    }
+
+    template <typename... Ts>
+	void plot(const Ts&... args)
+	{
+		auto t = std::make_tuple(args...);
+
+		std::cout << std::boolalpha;
+		std::cout << tuple_contains_type<ImageSize::CanvasType, decltype(styleString)>::value << std::endl;
+
+		// processArgs(args...);
+
+
+	}
+
 private:
     std::unique_ptr<Gnuplot> gnuplot_ = nullptr;
 };
 
-Qplot& operator<<(Qplot& qplot, const std::string& str)
+// Direct pass-through to gnuplot
+Qplot& operator<<(Qplot& qp, const std::string& str)
 {
-	*qplot.gnuplot_ << str;
-	return qplot;
+	*qp.gnuplot_ << str;
+	return qp;
 }
+
+// Main convenience method for one-liner plotting
+template <typename... Ts>
+void qplot(const Ts&... args)
+{
+    static Qplot qp;
+    qp.plot(args...);
+}
+
