5968c687667ae2f327f8af78a648307ef2492b75
Tom Ward <wardw@users.noreply.github.com>
2017-01-03 17:27:16 +0000

    More work on python examples to simplify usage
    Not there yet. It's still quite fiddly to change things

--- examples/example_python.cpp
+++ examples/example_python.cpp
@@ -18,15 +18,9 @@ struct Header
 	}
 };
 
-// Define a collection of functions that interpret our data types along dimensions that are meaningful
-// This isn't necessary for the examples (we could bundle inside any plot snippet), but useful later
-
-template <size_t N, typename T>
-void sendData(Subprocess<Python>& python, const T& obj);
-
-// Using the convention that a single dimension is a line of data
-template <>
-void sendData<0>(Subprocess<Python>& python, const std::vector<int>& obj)
+// Define a basic set of functions to interpret our data types along meaningful dimesions of interest
+// These examples use the convention that a line of data is a single dimension to python
+void sendData(Subprocess<Python>& python, const std::vector<int>& obj)
 {
 	for (int i=0; i<obj.size(); i++) {
 		python.data_out() << obj[i] << ' ';
@@ -34,89 +28,79 @@ void sendData<0>(Subprocess<Python>& python, const std::vector<int>& obj)
 	python.data_out() << endl;
 }
 
-template <>
-void sendData<1>(Subprocess<Python>& python, const std::vector<int>& obj)
-{
-    for (int i=0; i<obj.size(); i++) {
-		python.data_out() << i << ' ';
-	}
-	python.data_out() << endl;
-}
-
-template <>
-void sendData<0>(Subprocess<Python>& python, const std::map<int,int>& obj)
-{
-	for (auto elem : obj) {
-		python.data_out() << elem.second << ' ';
-	}
-	python.data_out() << endl;
-}
-
-template <>
-void sendData<1>(Subprocess<Python>& python, const std::map<int,int>& obj)
-{
-	for (auto elem : obj) {
-		python.data_out() << elem.first << ' ';
-	}
-	python.data_out() << endl;
-}
-
-template <>
-void sendData<0>(Subprocess<Python>& python, const std::vector<Point>& obj)
+void sendData(Subprocess<Python>& python, const std::vector<Point>& obj)
 {
 	for (auto elem : obj) {
 		python.data_out() << elem.x << ' ';
 	}
 	python.data_out() << endl;
-}
 
-template <>
-void sendData<1>(Subprocess<Python>& python, const std::vector<Point>& obj)
-{
 	for (auto elem : obj) {
 		python.data_out() << elem.y << ' ';
 	}
 	python.data_out() << endl;
 }
 
+void sendData(Subprocess<Python>& python, const std::map<int,int>& obj)
+{
+	for (auto elem : obj) {
+		python.data_out() << elem.first << ' ';
+	}
+	python.data_out() << endl;
+
+	for (auto elem : obj) {
+		python.data_out() << elem.second << ' ';
+	}
+	python.data_out() << endl;
+}
+
+// This exmample separates the plotting code depending on the data type.
+// The usual rules for overloading apply.  Unfortunately you must always provide
+// the `const T&` version anyway, even if it does nothing (todo)
 struct LineChart
 {
-	// Here we just match all T, where the usual rules for overloading apply should
-	// you want different behaviour for different types. Unfortunately you must always
-	// provide the `const T&` version anyway, even if it does nothing (todo)
-	template <typename T>
-	void operator()(Subprocess<Python>& python, const T& obj) const
+	void operator()(Subprocess<Python>& python, const vector<int>& obj) const
+	{
+        python <<
+R"(
+x = map(int, qp_data_in.readline().split())
+plt.plot(x, 'o-')
+)";
+		sendData(python, obj);
+	}
+
+	// template <typename T>
+	void operator()(Subprocess<Python>& python, const map<int,int>& obj) const
 	{
         python <<
 R"(
 x = map(int, qp_data_in.readline().split())
 y = map(int, qp_data_in.readline().split())
-
 plt.plot(x, y, 'o-')
-plt.plot(x, y, 'o-')
-)" << endl;
-
-		// Here (by exampe) we say there's really just one format we care about.
-		// So bundle here with the plotting code. See ScatterPlot for a more general approach
-		sendData<1>(python, obj);
-		sendData<0>(python, obj);
+)";
+		sendData(python, obj);
 	}
+
+	template <typename T>
+	void operator()(Subprocess<Python>& python, const T& obj) const {}
 };
 
-// This example includes both forms of `operator()`
-// Here we separate initialization code in a 'header' and leave the two
-// argument form to take the recurring behaviour on each data argument
+// This example uses both forms of `operator()`
+// We put initialization code in a 'header' and leave the two argument form
+// to take recurring behaviour applied to each data argument that's passed
 struct BarChart
 {
 	int numPlots = 1;
 
 	void operator()(Subprocess<Python>& python) const
 	{
-		python
-			<< "numPlots = " << numPlots << '\n'
-			<< "assert numPlots < 7\n"
-			<< "cols = ['b', 'g', 'r', 'c', 'm', 'y', 'k']\n"
-			<< "plotNum = 0\n";
+		python <<
+R"(
+numPlots =)" << numPlots << R"(
+assert numPlots < 7
+cols = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+plotNum = 0
+)";
 	}
 
 	template <typename T>
@@ -132,15 +116,13 @@ xPos = plotNum * width
 
 plt.bar([a+xPos for a in x], y, width-0.1, color=cols[plotNum])
 plotNum += 1
-)" << endl;
-
-		sendData<1>(python, obj);
-		sendData<0>(python, obj);
+)";
+		sendData(python, obj);
 	}
 };
 
 
-// In this example we separate the plotting code from the data
+// This example completely separates the plotting code from the data
 // This is useful in the more general case when a plot doesn't relate to a single object
 struct ScatterPlot
 {
@@ -153,18 +135,12 @@ R"(
 x = map(float, qp_data_in.readline().split())
 y = map(float, qp_data_in.readline().split())
 plt.scatter(x, y, s=)" << pointSize << R"()
-)" << endl;
+)";
 	}
 
-	// These functions essentially reduce to expressing defaults for how to interpret particular types
 	template <typename T>
 	void operator()(Subprocess<Python>& python, const T& obj) const {
-		sendData<0>(python, obj);
-	}
-
-	void operator()(Subprocess<Python>& python, const vector<Point>& obj) const {
-		sendData<0>(python, obj);
-		sendData<1>(python, obj);
+		sendData(python, obj);
 	}
 };
 
@@ -180,15 +156,42 @@ struct Show {
 	void operator()(Subprocess<Python>& python) const { python << "plt.show()\n"; }
 };
 
-using Data1D = variant<LineChart,BarChart,ScatterPlot>;
-using Data2D = variant<ScatterPlot>;
+// Original
+// using Data1Col    = variant<LineChart,ScatterPlot>;
+// using Data2Col    = variant<LineChart,BarChart>;
+// using ScatterData = variant<ScatterPlot>;
 
-// Consider all vectors as Data1D, and also std::map<int,int>
-template <typename T> struct has_styles<std::vector<T>>     { using type = Data1D; };
-template <>           struct has_styles<std::map<int,int>>  { using type = Data1D; };
-template <>           struct has_styles<std::vector<Point>> { using type = Data2D; };
+// // This maps our data types to the relevant code snippet 'styles' they may be called with
+// template <typename T> struct has_styles<std::vector<T>>     { using type = Data1Col; };
+// template <>           struct has_styles<std::map<int,int>>  { using type = Data2Col; };
+// template <>           struct has_styles<std::vector<Point>> { using type = ScatterData; };
 
-using MyStyles = std::tuple<Data1D,Data2D>;
+// using MyStyles = std::tuple<Data1Col,Data2Col,ScatterData>;
+
+
+// This maps our data types to the relevant code snippet 'styles' they may be called with
+template <typename T> struct has_styles<std::vector<T>>     { using type = variant<LineChart,ScatterPlot>; };
+template <>           struct has_styles<std::map<int,int>>  { using type = variant<LineChart,BarChart>; };
+template <>           struct has_styles<std::vector<Point>> { using type = variant<ScatterPlot>; };
+
+// Housekeeping. This sucks.
+using MyStyles = std::tuple<has_styles<vector<void>>::type,
+                            has_styles<map<int,int>>::type,
+                            has_styles<vector<Point>>::type>;
+
+// Aim (next)
+using MyTypes = std::tuple<vector<int>,vector<float>,map<int,int>,vector<Point>>;
+
+
+// Or cheat
+// But one requirement is that for any data object T that relates a set of 'style' variants (BarChart, LineChart etc), each
+// alternative must implement operator()(Subprocess<P>&, T obj) for each possible T regardless of which alternative
+// is the current chosen alternative. (Extends from the rules of std::variant)
+// using AllStyles = variant<LineChart,BarChart,ScatterPlot>;
+// template <typename T> struct qp::has_styles { using type = AllStyles; };
+// using MyStyles = std::tuple<AllStyles>;
+
+
 using Qp = Qplot<Python,MyStyles>;
 
 void example_python()
@@ -204,17 +207,21 @@ void example_python()
 	}
 
 	std::normal_distribution<> normal_dist(5, 1.5);
-	std::map<int,int> vals2;
+	std::poisson_distribution<> poisson_dist(3);
+	std::map<int,int> vals2, vals3;
 	n = 0;
 	while (n < 1000) {
 		vals2[normal_dist(gen)]++;
+		vals3[poisson_dist(gen)]++;
 		n++;
 	}
 
 	Qp qp(Header{});
-    qp.plot(vals1, vals2, Show());  // LineChart is automatically the default (the first variant alternative)
-    qp.plot(BarChart{2}, vals1, vals2, Show{});
-    qp.plot(LineChart{}, vals2, BarChart{1}, vals1, Show{});
+
+	// LineChart is automatically the default as the first variant alternative
+    qp.plot(vals1, vals2, vals3, Show());
+    qp.plot(BarChart{2}, vals2, vals3, Show{});
+    qp.plot(BarChart{1}, vals2, LineChart{}, vals3, Show{});
 
     // Or use a lambda
 	qp.plot(LineChart{}, vals2, [](Subprocess<Python>& py) {
@@ -237,14 +244,9 @@ void example_python()
 	qp.plot(Subplot{211}, ScatterPlot{50}, series1, series2, Subplot{212}, LineChart{}, series1, series2, Show{});
 
  	// But what if we don't want to use the default format? Then specify inline
- 	qp.plot(ScatterPlot{50}, [points](Subprocess<Python>& py) {
- 		sendData<1>(py, points);
- 		sendData<0>(py, points);
- 	},
- 	Show{});
+ 	// qp.plot(ScatterPlot{50}, [points](Subprocess<Python>& py) {
+ 	// 	sendData<1>(py, points);
+ 	// 	sendData<0>(py, points);
+ 	// },
+ 	// Show{});
 }
-
-// Left out
-// The one requirement is that given a data object T that relates a set of 'style' variants (BarChart, LineChart etc), each
-// variant alternative must implement operator()(Subprocess<P>&, T obj) for data object T regardless of which alternative
-// is the current chosen alternative. (Extends from the rules of std::variant)
