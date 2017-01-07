15ea9a57e5136989a575e20cc64ff4f626030623
Tom Ward <wardw@users.noreply.github.com>
2017-01-07 17:53:39 +0000

    Add basic test to test requirements for a function object

--- examples/test.cpp
+++ examples/test.cpp
@@ -9,6 +9,19 @@
 using namespace std;
 using namespace qp;
 
+static_assert(tuple_element_index<int,    std::tuple<int,char,float>>::value ==  0, "");
+static_assert(tuple_element_index<char,   std::tuple<int,char,float>>::value ==  1, "");
+static_assert(tuple_element_index<float,  std::tuple<int,char,float>>::value ==  2, "");
+static_assert(tuple_element_index<double, std::tuple<int,char,float>>::value == -1, "");
+
+static_assert(variant_element_index<int,    qp::variant<int,char,float>>::value ==  0, "");
+static_assert(variant_element_index<char,   qp::variant<int,char,float>>::value ==  1, "");
+static_assert(variant_element_index<float,  qp::variant<int,char,float>>::value ==  2, "");
+static_assert(variant_element_index<double, qp::variant<int,char,float>>::value == -1, "");
+
+static_assert( is_variant<variant<int,float>>::value,"");
+static_assert(!is_variant<std::nullptr_t>::value, "");
+
 struct CanvasStyle {
 	void operator()(Subprocess<Gnuplot>&) const {}
 };
@@ -29,19 +42,6 @@ static_assert(!is_object_style<DataObject,Gnuplot>::value, "");
 static_assert( is_canvas_style<CanvasStyle,Gnuplot>::value, "");
 static_assert(!is_canvas_style<ObjectStyle,Gnuplot>::value, "");
 static_assert(!is_canvas_style<DataObject,Gnuplot>::value, "");
-
-static_assert(tuple_element_index<int,    std::tuple<int,char,float>>::value ==  0, "");
-static_assert(tuple_element_index<char,   std::tuple<int,char,float>>::value ==  1, "");
-static_assert(tuple_element_index<float,  std::tuple<int,char,float>>::value ==  2, "");
-static_assert(tuple_element_index<double, std::tuple<int,char,float>>::value == -1, "");
-
-static_assert(variant_element_index<int,    std::variant<int,char,float>>::value ==  0, "");
-static_assert(variant_element_index<char,   std::variant<int,char,float>>::value ==  1, "");
-static_assert(variant_element_index<float,  std::variant<int,char,float>>::value ==  2, "");
-static_assert(variant_element_index<double, std::variant<int,char,float>>::value == -1, "");
-
-static_assert( is_variant<variant<int,float>>::value,"");
-static_assert(!is_variant<std::nullptr_t>::value, "");
 
 struct A {};
 struct B {};
@@ -64,6 +64,26 @@ static_assert( has_related_style<vector<int>>::value, "");
 static_assert( has_related_style<vector<float>>::value, "");
 static_assert(!has_related_style<char>::value, "");
 
+
+// Function object requirements
+
+struct Snippet
+{
+	// Required
+	Snippet() {}
+    Snippet& operator=(const Snippet& rhs) { return *this; }
+    Snippet(const Snippet& rhs) {}
+
+    void operator()(Subprocess<Python>& python) const {}
+};
+
+template <> struct has_styles<int> { using type = variant<Snippet>; };
+using RequirementsTestTypes = std::tuple<int>;
+
 void testing()
 {
+	Qplot<Python,RequirementsTestTypes> qp;
+
+	qp.plot(Snippet{});
+
 }
